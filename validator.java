ad1838101d3da8f18d1b3e20fe99c7b6b6e44920
Abhishek Narsing <34853240+Abhisheknarsing@users.noreply.github.com>
2020-08-30 17:37:45 +0530

    Create validator.java

---
+++ validator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class HelloWorld
+{
+
+     public static void main(String []args)
+     {
+        System.out.println("Enter Card Number\n");
+        String s = new Scanner(System.in).nextLine();
+        System.out.println(validataCard(s));
+     }
+     public static boolean validataCard(String sixteenDigitString)
+     {
+        int sum = 0;
+        if (sixteenDigitString.length() != 16){
+            return false;
+        }
+        else{
+            for (int temp=0;temp<16;temp++){ 
+                if(temp%2==0){    
+                    int value = Character.getNumericValue(sixteenDigitString.charAt(temp));
+                    value = value+value;
+                    if(value>9){
+                        sum = sum+1+value%10;
+                    }
+                    else{
+                        sum=sum+value;
+                    }
+                }
+                else{     
+                    sum=sum+Character.getNumericValue(sixteenDigitString.charAt(temp));
+                }
+            }            
+        }
+        return sum%10==0;
+     }
+}
